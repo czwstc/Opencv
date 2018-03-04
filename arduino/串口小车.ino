0f42c1d95c2c9dea4f634f50044d70509622c2bd
Zwei <czwstc@Zweis-MacBook-Pro.local>
2018-03-04 20:33:34 +0800

    修改为红外

--- arduino/串口小车.ino
+++ arduino/串口小车.ino
@@ -1,26 +1,34 @@
 #include<Servo.h>
+#include <IRremote.h>
 
+int recvPin = 13;//定义红外接收器的引脚为3
+IRrecv irrecv(recvPin);
+decode_results results;
 Servo a,b,c,d;
 int i,j;
+long KEY_CODE_Left = 0xFF22DD;
+long KEY_CODE_Right = 0xFF02FD;
+long KEY_CODE_ZERO = 0xFFC23D;
 String input = "";
 void setup() 
 {
+  irrecv.enableIRIn(); // 初始化红外接收器
   Serial.begin(9600);
   for(i=2;i<9;i++)
   {if(i==2||i==4||i==7||i==8)
   {pinMode(i,OUTPUT); }}
   a.attach(5);b.attach(6);c.attach(9);d.attach(10);
-  a.write(180);b.write(180);c.write(180);d.write(180);
+  stop();
 }
 void up() //往前
 {
   a.write(180);b.write(180);c.write(180);d.write(180);
-  digitalWrite(2,HIGH);digitalWrite(4,HIGH);digitalWrite(7,HIGH);digitalWrite(8,HIGH);
+  analogWrite(2,200);analogWrite(4,200);analogWrite(7,200);analogWrite(8,200);
 }
-void down()//往后
+
+void stop() //stop
 {
-  a.write(180);b.write(180);c.write(180);d.write(180);
-    digitalWrite(2,LOW);digitalWrite(4,LOW);digitalWrite(7,LOW);digitalWrite(8,LOW);
+  a.write(0);b.write(0);c.write(0);d.write(0);
 }
 void left()//往左
 {
@@ -32,165 +40,35 @@ void right()//往右
   a.write(180);b.write(180);c.write(180);d.write(180);
   digitalWrite(2,HIGH);digitalWrite(4,LOW);digitalWrite(7,LOW);digitalWrite(8,HIGH);
 }
-void left_and_up()//左上
-{
-  a.write(0);b.write(180);c.write(180);d.write(0);
-  digitalWrite(2,LOW);digitalWrite(4,HIGH);digitalWrite(7,HIGH);digitalWrite(8,LOW);
-}
-void left_and_down()
-{
-  a.write(180);b.write(0);c.write(0);d.write(180);
-  digitalWrite(2,LOW);digitalWrite(4,HIGH);digitalWrite(7,HIGH);digitalWrite(8,LOW);
-}
-void right_and_up()
-{
-  a.write(180);b.write(0);c.write(0);d.write(180);
-  digitalWrite(2,HIGH);digitalWrite(4,HIGH);digitalWrite(7,HIGH);digitalWrite(8,HIGH);
-}
-void right_and_down()
-{
-  a.write(0);b.write(180);c.write(180);d.write(0);
-  digitalWrite(2,LOW);digitalWrite(4,LOW);digitalWrite(7,LOW);digitalWrite(8,LOW);
-}
-void clock_wise()
-{
-  a.write(180);b.write(180);c.write(180);d.write(180);
-  digitalWrite(2,HIGH);digitalWrite(4,LOW);digitalWrite(7,HIGH);digitalWrite(8,LOW);
-}
-void anti_clock_wise()
-{
-  a.write(180);b.write(180);c.write(180);d.write(180);
-  digitalWrite(2,LOW);digitalWrite(4,HIGH);digitalWrite(7,LOW);digitalWrite(8,HIGH);
-}
-void stop() //stop
-{
-  a.write(0);b.write(0);c.write(0);d.write(0);
-}
-
 void loop() 
 { 
-    //receive command from serial port
-  while (Serial.available()>0) //读取串口数据
+   if (irrecv.decode(&results)) 
   {
-    input += char(Serial.read());
-    delay(2);
+    Serial.println(results.value, HEX);//以16进制换行输出接收代码
+    Serial.println();//为了便于观看输出结果增加一个空行
+    irrecv.resume(); // 接收下一个值
   }
   if (input.length()>0) //output the command
   {
     Serial.println(input);
   }
 
-  if(input == "0")
+  if(results.value == KEY_CODE_ZERO )
   {
     stop();
   }
-  if(input == "1")
+    if(results.value == KEY_CODE_Right)
   {
-    right();
-    delay(3000);
+    right();    
+    delay(5000);
     stop();
   }
-
-/*
-  //按下CH-键的事件
-  if (results.value == KEY_CODE_CHL ) 
+      if(results.value == KEY_CODE_Left)
   {
-    up();
-    longPressKey = KEY_CODE_CHL;
+    left();    
+    delay(5000);
+    stop();
   }
-  //长按CH-键的事件
-  if ( results.value == KEY_CODE_LONG_PRESS && longPressKey == KEY_CODE_CHL)
-  {
-    up();
-  }//按下CH键的事件
-  if (results.value == KEY_CODE_CH) 
-  {
-    down();
-    longPressKey = KEY_CODE_CH;
-  }
-  //长按CH键的事件
-  if ( results.value == KEY_CODE_LONG_PRESS && longPressKey == KEY_CODE_CH)
-  {
-    down();
-  }//按下CH+键的事件
-  if (results.value == KEY_CODE_CHH ) 
-  {
-    left();
-    longPressKey = KEY_CODE_CHH;
-  }
-  //长按CH+键的事件
-  if ( results.value == KEY_CODE_LONG_PRESS && longPressKey == KEY_CODE_CHH)
-  {
-    left();
-  }//按下|◀◀键的事件
-  if (results.value == KEY_CODE_TUI ) 
-  {
-    right();
-    longPressKey = KEY_CODE_TUI;
-  }
-  //长按|◀◀键的事件
-  if ( results.value == KEY_CODE_LONG_PRESS && longPressKey == KEY_CODE_TUI)
-  {
-    right();
-  }//按下▶▶|键的事件
-  if (results.value == KEY_CODE_JIN ) 
-  {
-    left_and_up();
-    longPressKey = KEY_CODE_JIN;
-  }
-  //长按▶▶|键的事件
-  if ( results.value == KEY_CODE_LONG_PRESS && longPressKey == KEY_CODE_JIN)
-  {
-    left_and_up();
-  }//按下▶||键的事件
-  if (results.value == KEY_CODE_XIA ) 
-  {
-    left_and_down();
-    longPressKey = KEY_CODE_XIA;
-  }
-  //长按▶||键的事件
-  if ( results.value == KEY_CODE_LONG_PRESS && longPressKey == KEY_CODE_XIA)
-  {
-    left_and_down();
-  }//按下"-"键的事件
-  if (results.value == KEY_CODE_L ) 
-  {
-    right_and_up();
-    longPressKey = KEY_CODE_L;
-  }
-  //长按"-"键的事件
-  if ( results.value == KEY_CODE_LONG_PRESS && longPressKey == KEY_CODE_L)
-  {
-    right_and_up();
-  }//按下"+"键的事件
-  if (results.value == KEY_CODE_H ) 
-  {
-    right_and_down();
-    longPressKey = KEY_CODE_H;
-  }
-  //长按"+"键的事件
-  if ( results.value == KEY_CODE_LONG_PRESS && longPressKey == KEY_CODE_H)
-  {
-    right_and_down();
-  }//按下EQ键的事件
-  if (results.value == KEY_CODE_EQ ) {
-    clock_wise();
-    longPressKey = KEY_CODE_EQ;
-  }
-  //长按EQ键的事件
-  if ( results.value == KEY_CODE_LONG_PRESS && longPressKey == KEY_CODE_EQ)
-  {
-    clock_wise();
-  }//按下0键的事件
-  if (results.value == KEY_CODE_ZERO ) {
-    anti_clock_wise();
-    longPressKey = KEY_CODE_ZERO;
-  }
-  //长按0键的事件
-  if ( results.value == KEY_CODE_LONG_PRESS && longPressKey == KEY_CODE_ZERO)
-  {
-    anti_clock_wise();
-  }
-  */
+  
     input = "";
 }
